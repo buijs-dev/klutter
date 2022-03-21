9643cdd59449eef2067e0b935ba32747c84b063f
buijs <jjgbuijs@gmail.com>
2022-03-21 18:17:43 +0100

    Add Klutter JVM class to retrieve properties from hidden files.

---
+++ klutter-core/core/src/main/kotlin/dev/buijs/klutter/core/KlutterKey.kt
@@ -0,0 +1,52 @@
+@file:JvmName("Klutter")
+package dev.buijs.klutter.core
+
+import org.gradle.api.Project
+import java.io.File
+
+private const val secretname = "klutter-secrets.properties"
+
+fun Project.key(name: String) = keys(toLocation(this))[name]
+
+fun secrets(project: Project) = Secrets(keys(toLocation(project)))
+
+fun secrets(location: String) = Secrets(keys(File(location)))
+
+class Secrets(
+    private val properties: HashMap<String, String>,
+) {
+
+    fun get(key: String) = properties[key]
+
+}
+
+internal fun keys(location: File): HashMap<String, String> {
+
+    val secrets = location.listFiles()
+        ?.firstOrNull { it.name == secretname }
+        ?: throw KlutterGradleException("File klutter-secrets.properties could not be located in $location")
+
+    val properties = HashMap<String, String>()
+
+    secrets.forEachLine {
+        val pair = it.split("=")
+        if(pair.size == 2){
+            properties[pair[0]] = pair[1]
+        }
+    }
+
+   return properties
+
+}
+
+internal fun toLocation(project: Project): File {
+    val rootProject = project.rootProject.rootDir
+    //Android package has it's own settings file due to Flutter requirements.
+    //This means that rootDir is pointing to android folder as root package
+    //and not the top level when called from the android or app build.gradle.
+    return if(rootProject.absolutePath.endsWith("android")){
+        rootProject.resolve("..").resolve("buildSrc")
+    } else {
+        rootProject.resolve("buildSrc")
+    }
+}

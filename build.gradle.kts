cbde3523a13643d2df60e1711cf85bab882f754d
buijs <jjgbuijs@gmail.com>
2022-06-07 21:01:06 +0200

    Bump kover plugin to 0.5.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     kotlin("jvm") version "1.6.10"
     id("org.jetbrains.dokka") version "1.6.10"
-    id("org.jetbrains.kotlinx.kover") version "0.5.0"
+    id("org.jetbrains.kotlinx.kover") version "0.5.1"
 }
 
 subprojects {

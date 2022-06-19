65a5b313183201315930f2de9b036ff84fa9ff7a
buijs <jjgbuijs@gmail.com>
2022-06-19 14:48:15 +0200

    Cleanup and testcoverage.

--- packages/core/src/test/groovy/dev/buijs/klutter/core/shared/StringUtilsSpec.groovy
+++ packages/core/src/test/groovy/dev/buijs/klutter/core/shared/StringUtilsSpec.groovy
@@ -1,6 +1,5 @@
 package dev.buijs.klutter.core.shared
 
-
 import spock.lang.Specification
 
 class StringUtilsSpec extends Specification {
@@ -8,6 +7,11 @@ class StringUtilsSpec extends Specification {
     def "[unwrapFromList] returns value if no match found"(){
         expect:
         "List<>" == StringUtilsKt.unwrapFromList("List<>")
+    }
+
+    def "[findClassName] returns null if match is not found"(){
+        expect:
+        null == StringUtilsKt.findClassName("")
     }
 
 }

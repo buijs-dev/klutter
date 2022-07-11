61b26ac3b08e46b9058c3c2d1fdafb0dec4b6869
buijs <jjgbuijs@gmail.com>
2022-07-11 12:19:56 +0200

    Move to lib and use lib-build.

---
+++ lib/test/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id("klutter")
+    id("klutter-java")
+    id("klutter-test")
+}
+
+sourceSets {
+    main {
+        java {
+            srcDirs("${projectDir.absolutePath}/src/main/kotlin")
+        }
+    }
+
+    test {
+        java {
+            srcDirs("${projectDir.absolutePath}/src/test/kotlin")
+        }
+    }
+}
+
+sonarqube {
+    isSkipProject = true
+}
+
+dependenciesTest {
+    implementation.forEach {
+        dependencies.add("testImplementation", it)
+    }
+}
+
+dependenciesJava {
+    implementation.forEach {
+        dependencies.add("implementation", it)
+    }
+}
+
+dependencies {
+    implementation(gradleApi())
+    implementation(gradleTestKit())
+}

8d8df5a5acb96ff11cd51b992c899561c94fad1b
buijs <jjgbuijs@gmail.com>
2023-04-23 12:24:21 +0200

    Include BOM, Kommand and Test-Integration.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,12 @@
 // Project modules
 include(":lib:annotations")
+include(":lib:bill-of-materials")
 include(":lib:compiler")
 include(":lib:flutter-engine")
 include(":lib:gradle")
 include(":lib:jetbrains")
 include(":lib:kompose")
+include(":lib:kommand")
 include(":lib:kore")
 include(":lib:tasks")
 // Internal Testing library
@@ -12,5 +14,7 @@ include(":lib-test")
 // Internal workload for testing annotations processor
 include(":test-ksp")
 include(":test-ksp:platform")
+// Test generating a new project using the local modules
+include(":test-integration")
 // Internal build properties
 includeBuild("lib-build")

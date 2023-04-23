40085945c548da5005ed22bd6e37723561c662f0
buijs <jjgbuijs@gmail.com>
2023-04-23 12:40:04 +0200

    Add buildscripts.

---
+++ tools/publish_bom.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#stop script on failure
+set -e
+
+cd ".."
+
+echo " ____________
+< Publishing Klutter bill-of-materials modules >
+ ------------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||"
+
+echo "\0/ Klutter: step: build bill-of-materials modules"
+echo "------------------"
+./gradlew clean -p "lib/bill-of-materials"
+./gradlew build -p "lib/bill-of-materials"
+
+echo "\0/ Klutter: step: publish bill-of-materials modules"
+echo "------------------"
+./gradlew publish -p "lib/bill-of-materials"

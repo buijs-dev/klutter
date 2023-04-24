8cf1a038c1ceb7b3d30bc619bb61885587187765
buijs <jjgbuijs@gmail.com>
2023-04-24 18:44:16 +0200

    Add Config to ProjectBuilderOptions to avoid having to lookup a File multiple times.

--- test-integration/src/main/kotlin/dev/buijs/klutter/test/integration/GenerateTestProject.kt
+++ test-integration/src/main/kotlin/dev/buijs/klutter/test/integration/GenerateTestProject.kt
@@ -1,5 +1,7 @@
 package dev.buijs.klutter.test.integration
 
+import dev.buijs.klutter.kore.project.Dependencies
+import dev.buijs.klutter.kore.project.Config
 import dev.buijs.klutter.kore.project.Project
 import dev.buijs.klutter.kore.project.plugin
 import dev.buijs.klutter.tasks.input.toGroupName
@@ -10,32 +12,22 @@ import dev.buijs.klutter.tasks.project.ProjectBuilderTask
 import java.nio.file.Files.createTempDirectory
 import kotlin.io.path.absolutePathString
 import kotlin.io.path.createDirectory
-import kotlin.io.path.createFile
-import kotlin.io.path.writeText
 
 fun generatePlugin(): Project {
-
-    val root = createTempDirectory("")
-        .also {
-            it.resolve("my_custom_plugin")
-                .also { dir -> dir.createDirectory() }
-                .resolve("klutter.yaml")
-                .also { yaml -> yaml.createFile() }
-                .also { yaml -> yaml.writeText("""
-                    |dependencies:
-                    |    klutter: 'https://github.com/buijs-dev/klutter-dart.git@develop'
-                    |    klutter_ui: 'https://github.com/buijs-dev/klutter-dart-ui.git@develop'
-                    |    squint_json: 'https://github.com/buijs-dev/squint.git@develop'
-                """.trimMargin()) }
-    }
+    val root = createTempDirectory("").also {
+        it.resolve("my_custom_plugin")
+            .also { dir -> dir.createDirectory() } }
 
     ProjectBuilderTask(
         ProjectBuilderOptions(
             rootFolder = toRootFolder(root.absolutePathString()),
             pluginName = toPluginName("my_custom_plugin"),
-            groupName = toGroupName("com.example.apps")))
+            groupName = toGroupName("com.example.apps"),
+            config = Config(
+                dependencies = Dependencies(
+                    klutter = "https://github.com/buijs-dev/klutter-dart.git@develop",
+                    klutterUI = "https://github.com/buijs-dev/klutter-dart-ui.git@develop",
+                    squint = "https://github.com/buijs-dev/squint.git@develop"))))
         .run()
-
     return root.resolve("my_custom_plugin").toFile().plugin()
-
 }

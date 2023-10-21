18e964e875f13e4425eb47aed8c3ba019d0c8da1
Gillian <72627535+buijs-dev@users.noreply.github.com>
2023-10-21 12:52:57 +0200

    Fix kradle GitHub Workflow.

--- lib/kradle/res/kradlew.bat
+++ lib/kradle/res/kradlew.bat
@@ -67,7 +67,7 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=\kradle\build\libs\kradle-wrapper.jar
+set CLASSPATH=\lib\kradle-wrapper.jar
 
 @rem Execute kradlew
 "%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %KRADLEW_OPTS%  -classpath "%CLASSPATH%" dev.buijs.klutter.kradle.MainKt %*
